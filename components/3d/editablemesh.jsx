6b03488900d72097723280dac11b4233b9ee3fdd
Leah Sandacz <582076+turtles@users.noreply.github.com>
2024-10-25 22:09:19 -0700

    Change model for editable mesh

--- components/3d/editablemesh.jsx
+++ components/3d/editablemesh.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react'
 import { useFrame } from '@react-three/fiber'
+import { useGLTF } from '@react-three/drei';
 import * as THREE from "three";
 
 const EditableMesh = (props) => {
@@ -8,21 +9,28 @@ const EditableMesh = (props) => {
     const [clicked, click] = useState(false);
 
     useFrame((state, delta) => {
-        ref.current.rotation.x += delta;
+        ref.current.rotation.x += delta * 0.2;
         ref.current.rotation.y += delta;
-        ref.current.rotation.z += delta;
+        // ref.current.rotation.z += delta;
     });
+
+    const { nodes, materials } = useGLTF('/utah_teapot.glb');
+
 
     return (
         <mesh
             {...props}
+            geometry={nodes.teapot.geometry}
+            morphTargetDictionary={nodes.teapot.morphTargetDictionary}
+            morphTargetInfluences={nodes.teapot.morphTargetInfluences}
             ref={ref}
             scale={clicked ? 1.5 : 1}
             onClick={(event) => click(!clicked)}
             onPointerOver={(event) => hover(true)}
             onPointerOut={(event) => hover(false)}>
-            <boxGeometry />
-            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+            <meshStandardMaterial color={hovered ? 'hotpink' : 'white'}
+                wireframe={props.isWireframe}
+            />
         </mesh>
     )
 }
