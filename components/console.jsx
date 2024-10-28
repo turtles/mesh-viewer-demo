853e7ae8334b4e0d53fd8841e84d01eaa195dbf8
Leah Sandacz <582076+turtles@users.noreply.github.com>
2024-10-28 02:56:45 -0700

    Prevent array mutation and add fade effect

--- components/console.jsx
+++ components/console.jsx
@@ -4,7 +4,7 @@ import styles from '../styles/EditorPanel.module.css';
 const Console = ({ commands }) => {
     return (
         <div className={styles.console}>
-            {commands.reverse().map((command, index) => (<span key={index}>{command.label}</span>))}
+            {[...commands].reverse().map((command, index) => (<span style={{ opacity: 1 / (index) }} key={index}>{command.label}</span>))}
         </div>
     )
 }
